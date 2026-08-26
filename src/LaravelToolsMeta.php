30841d2503270f9721d24c029014f1a789d70afa
Francisco Blanco <blancofrancisco34@gmail.com>
2026-08-26 12:26:33 -0500

    update sendAction Meta

--- src/LaravelToolsMeta.php
+++ src/LaravelToolsMeta.php
@@ -7,39 +7,53 @@ class LaravelToolsMeta
 {
     private string $pixelId;
     private string $accessToken;
+    private string $apiVersion;
 
     public function __construct($config = [])
     {
         $this->pixelId = isset($config['pixelId']) ? $config['pixelId'] : null;
         $this->accessToken = isset($config['accessToken']) ? $config['accessToken'] : null;
+        $this->apiVersion = isset($config['apiVersion']) ? $config['apiVersion'] : 'v26.0';
     }
 
     function sendAction($data = [
         "event_name" => "",
+        "event_time" => null,
         "action_source" => "website",
+        "event_source_url" => null,
+        "event_id" => null,
         "user_data" => [],
         "custom_data" => [],
     ])
     {
-        $pixelId = $this->pixelId;
-        $accessToken = $this->accessToken;
+        $url = "https://graph.facebook.com/{$this->apiVersion}/{$this->pixelId}/events";
 
-        $url = "https://graph.facebook.com/v23.0/{$pixelId}/events?access_token={$accessToken}";
-
-        $data = [
+        $payload = [
             "data" => [
                 [
-                    "event_name" => $data['event_name'],
-                    "event_time" => time(),
-                    "action_source" => $data['action_source'],
-
-                    "user_data" => $data['user_data'],
-
-                    "custom_data" => $data['custom_data'],
-
+                    "event_name" => $data['event_name'] ?? '',
+                    "event_time" => $data['event_time'] ?? time(),
+                    "action_source" => $data['action_source'] ?? 'website',
+                    "event_source_url" => $data['event_source_url'] ?? null,
+                    "event_id" => $data['event_id'] ?? null,
+                    "user_data" => $data['user_data'] ?? [],
+                    "custom_data" => $data['custom_data'] ?? [],
                 ]
-            ]
+            ],
+            "access_token" => $this->accessToken,
         ];
+
+        $json = json_encode($payload);
+
+        if ($json === false) {
+            return [
+                "http_code" => null,
+                "curl_errno" => null,
+                "curl_error" => "JSON ERROR: " . json_last_error_msg(),
+                "payload" => null,
+                "response" => null,
+            ];
+        }
 
         $curl = curl_init($url);
 
@@ -49,13 +63,23 @@ class LaravelToolsMeta
             CURLOPT_HTTPHEADER => [
                 "Content-Type: application/json"
             ],
-            CURLOPT_POSTFIELDS => json_encode($data)
+            CURLOPT_POSTFIELDS => $json,
         ]);
 
         $response = curl_exec($curl);
 
+        $curlError = curl_error($curl);
+        $curlErrno = curl_errno($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
         curl_close($curl);
 
-        return json_encode($response);
+        return [
+            "http_code" => $httpCode,
+            "curl_errno" => $curlErrno,
+            "curl_error" => $curlError,
+            "payload" => json_decode($json, true),
+            "response" => json_decode($response ?: '', true),
+        ];
     }
 }
